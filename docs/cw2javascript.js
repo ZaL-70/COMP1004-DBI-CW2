af0c0c558a5a7bb8d03fdc78023e31b5e35e98ed
psysa19 <psysa19@exmail.nottingham.ac.uk>
2024-04-15 20:57:18 +0100

    supabase testing from live server

--- docs/cw2javascript.js
+++ docs/cw2javascript.js
@@ -1,5 +1,22 @@
 // After install the supabase-js module
-import { createClient } from
-'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 // Create a single supabase client for interacting with your database
-const supabase = createClient('https://digqlsccmsppgbanysko.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRpZ3Fsc2NjbXNwcGdiYW55c2tvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI4NTA5MzcsImV4cCI6MjAyODQyNjkzN30.JFGokF0fDv9XndLlUIEwSu6b3U02gzY3dd0qhyr6Znw')
+const supabase = createClient("https://digqlsccmsppgbanysko.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRpZ3Fsc2NjbXNwcGdiYW55c2tvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI4NTA5MzcsImV4cCI6MjAyODQyNjkzN30.JFGokF0fDv9XndLlUIEwSu6b3U02gzY3dd0qhyr6Znw");
+
+// Fetch data from the table
+async function fetchData() {
+    const { data, error } = await supabase.from('Vehicles').select();
+    console.log('Fetched data:', data);
+}
+// Call the fetchData function to retrieve data
+fetchData();
+    
+
+// const button = document.querySelector("button");
+// button.addEventListener("click", updateParagraph);
+
+// const paragraph = document.getElementById("output");
+
+// function updateParagraph() {
+//     paragraph.textContent = "I did this";
+// }
