2101da1cfc0f270d9986fe4919b382e741da7256
psysa19 <psysa19@exmail.nottingham.ac.uk>
2024-04-16 23:52:30 +0100

    testing vehicle search

--- docs/cw2javascript.js
+++ docs/cw2javascript.js
@@ -3,15 +3,15 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 // Create a single supabase client for interacting with your database
 const supabase = createClient("https://digqlsccmsppgbanysko.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRpZ3Fsc2NjbXNwcGdiYW55c2tvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI4NTA5MzcsImV4cCI6MjAyODQyNjkzN30.JFGokF0fDv9XndLlUIEwSu6b3U02gzY3dd0qhyr6Znw");
 
-const submitBtn = document.getElementById("submitBtn");
-submitBtn.addEventListener("click", updateResults); // Add listener to button
+const btnPeopleQuery = document.getElementById("btnPeopleQuery");
+btnPeopleQuery.addEventListener("click", updatePeopleResults); // Add listener to people query button
 
-async function updateResults() {
+async function updatePeopleResults() {
      let found = false;
      let driverName = "";
      let licenseNum = "";
      // Remove all existing search results
-     const existingResults = document.querySelectorAll("#searchResult");
+     const existingResults = document.querySelectorAll("#searchResultPeople");
      existingResults.forEach(result => {
           result.remove();
      });
@@ -19,10 +19,10 @@ async function updateResults() {
      // Declare result variables to append to DOM
      const mainSect = document.querySelector("main");
      // Get input query for name
-     const driverNameElement = document.getElementById("driverName"); // Get user input
+     const driverNameElement = document.getElementById("txtDriverName"); // Get user input
      driverName = driverNameElement.value.trim().toLowerCase();
      // Get input query for license number
-     const licenseNumElement = document.getElementById("licenseNum"); // Get user input
+     const licenseNumElement = document.getElementById("txtLicenseNum"); // Get user input
      licenseNum = licenseNumElement.value.trim().toLowerCase();
  
      const { data: arrQuery, error: nameSelError } = await supabase
@@ -34,9 +34,9 @@ async function updateResults() {
           let pLicenseNumLower = pQuery.LicenseNumber.toLowerCase();
           if ((pNameLower.includes(driverName) && driverName !== "") || (pLicenseNumLower.includes(licenseNum) && licenseNum !== "")) {
                found = true;
-               const { data: arrPeople, error: allSelError } = await supabase.from("People").select().eq("Name", pQuery.Name || "LicenseNumber", pQuery.LicenseNum);
+               const { data: arrPeople, error: allSelError } = await supabase.from("People").select().eq("Name", pQuery.Name || "LicenseNumber", pQuery.LicenseNumber);
                const results = document.createElement("ul");
-               results.id = "searchResult";
+               results.id = "searchResultPeople";
                
                for (const person of arrPeople) {
                     // Create <li> elements for each field and populate them with the field value
@@ -53,7 +53,58 @@ async function updateResults() {
 
      if (found === false) {
           const results = document.createElement("p");
-          results.id = "searchResult";
+          results.id = "searchResultPeople";
+          results.textContent = "No matches found";
+          mainSect.appendChild(results); // Append corresponding results
+     } 
+ }
+
+ const btnVehicleQuery = document.getElementById("btnVehicleQuery");
+btnVehicleQuery.addEventListener("click", updateVehicleResults); // Add listener to people vehicle
+
+async function updateVehicleResults() {
+     let found = false;
+     let regNum = "";
+     // Remove all existing search results
+     const existingResults = document.querySelectorAll("#searchResultVehicle");
+     existingResults.forEach(result => {
+          result.remove();
+     });
+
+     // Declare result variables to append to DOM
+     const mainSect = document.querySelector("main");
+     // Get input query for registration number
+     const driverNameElement = document.getElementById("txtRegNum"); // Get user input
+     driverName = driverNameElement.value.trim().toLowerCase();
+ 
+     const { data: arrQuery, error: nameSelError } = await supabase
+          .from("Vehicles")
+          .select("VehicleID"); // Get name/license number data
+     // Check if the input is a substring of any name
+     for (const pQuery of arrQuery) {
+          let vRegNum = pQuery.VehicleID.toLowerCase();
+          if (vRegNum.includes(regNum) && regNum !== "") {
+               found = true;
+               const { data: arrVehicles, error: allSelError } = await supabase.from("Vehicles").select().eq("Vehicle", pQuery.VehicleID);
+               const results = document.createElement("ul");
+               results.id = "searchResultVehicle";
+               
+               for (const vehicle of arrVehicles) {
+                    // Create <li> elements for each field and populate them with the field value
+                    const fields = ["VehicleID", "Make", "Model", "Colour", "OwnerID"];
+                    fields.forEach(field => {
+                         const li = document.createElement("li");
+                         li.textContent = `${field}: ${vehicle[field]}`; // Populate <li> with field value
+                         results.appendChild(li); // Append <li> to <ul>
+                    });
+               }
+               mainSect.appendChild(results); // Append search result <div> to main section
+          }
+     }
+
+     if (found === false) {
+          const results = document.createElement("p");
+          results.id = "searchResultVehicle";
           results.textContent = "No matches found";
           mainSect.appendChild(results); // Append corresponding results
      } 
