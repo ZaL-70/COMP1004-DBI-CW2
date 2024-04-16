1c48e33ab554c27d4671da2948b885dbf2b86f39
psysa19 <psysa19@exmail.nottingham.ac.uk>
2024-04-17 00:55:45 +0100

    testing alternative method

--- docs/cw2javascript.js
+++ docs/cw2javascript.js
@@ -3,15 +3,15 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 // Create a single supabase client for interacting with your database
 const supabase = createClient("https://digqlsccmsppgbanysko.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRpZ3Fsc2NjbXNwcGdiYW55c2tvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI4NTA5MzcsImV4cCI6MjAyODQyNjkzN30.JFGokF0fDv9XndLlUIEwSu6b3U02gzY3dd0qhyr6Znw");
 
-const btnPeopleQuery = document.getElementById("btnPeopleQuery");
-if (btnPeopleQuery) {
-    btnPeopleQuery.addEventListener("click", updatePeopleResults);
-}
+// const btnPeopleQuery = document.getElementById("btnPeopleQuery");
+// if (btnPeopleQuery) {
+//     btnPeopleQuery.addEventListener("click", updatePeopleResults);
+// }
 
-const btnVehicleQuery = document.getElementById("btnVehicleQuery");
-if (btnVehicleQuery) {
-    btnVehicleQuery.addEventListener("click", updateVehicleResults);
-}
+// const btnVehicleQuery = document.getElementById("btnVehicleQuery");
+// if (btnVehicleQuery) {
+//     btnVehicleQuery.addEventListener("click", updateVehicleResults);
+// }
 
 async function updatePeopleResults() {
      let found = false;
