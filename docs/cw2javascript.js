fe7af545a8286fb419e000d0d5a7c872b0c2efbd
psysa19 <psysa19@exmail.nottingham.ac.uk>
2024-04-17 20:16:07 +0100

    js = awful

--- docs/cw2javascript.js
+++ docs/cw2javascript.js
@@ -124,7 +124,7 @@ async function updateVehicleResults() {
      } 
  }
 
-async function addVehicleData() {
+function addVehicleData() {
      let vID = null;
      let vMake = null;
      let vModel = null;
@@ -148,7 +148,7 @@ async function addVehicleData() {
           return;
      }
 
-     const exists = await checkPersonExists(vOwnerID)
+     const exists = checkPersonExists(vOwnerID)
      if(exists === true) { // Add vehicle data if its owner exists
           insertVehicle(vID, vMake, vModel, vColour, vOwnerID);
           console.log("vowner id existed");
@@ -173,7 +173,7 @@ async function insertVehicle(vehicleID, vehicleMake, vehicleModel, vehicleColour
      });
 }
 
-async function addPersonData() {
+function addPersonData() {
      let pID = null;
      let pName = null;
      let pAddress = null;
@@ -200,7 +200,7 @@ async function addPersonData() {
           return;
      }
 
-     const exists = await checkPersonExists(pID)
+     const exists = checkPersonExists(pID)
      if(exists === false) { // Add vehicle data if its owner exists
           insertPerson(pID, pName, pAddress, pDOB, pLicenseNum, pExpiryDate);
           console.log("Person id didnt existed");
